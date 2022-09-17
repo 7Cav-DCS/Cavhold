a18f2cf1fad024c7777983a428d102fac0d11e12
Pingoo <>
2022-09-16 23:03:02 -0400

    Disable ATIS at Anapa for FC3 players (cannot turn off ATIS and very loud)

--- l10n/DEFAULT/ATIS.lua
+++ l10n/DEFAULT/ATIS.lua
@@ -1,4 +1,7 @@
 -- ATIS Anapa Airport on 143.00 MHz AM.
-atisAnapa=ATIS:New(AIRBASE.Caucasus.Anapa_Vityazevo, 143.00)
-atisAnapa:SetTowerFrequencies({121, 250})
-atisAnapa:Start()
+
+-- TODO: Revisit to enable ATIS but respect FC3 radio
+-- Disabling due to FC3 aircraft not being able to turn ATIS off
+-- atisAnapa=ATIS:New(AIRBASE.Caucasus.Anapa_Vityazevo, 143.00)
+-- atisAnapa:SetTowerFrequencies({121, 250})
+-- atisAnapa:Start()
